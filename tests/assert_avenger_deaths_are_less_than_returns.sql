f29f01a063265b0a2d39f9458096b486f42c5369
jadesola98 <77394784+jadesola98@users.noreply.github.com>
2022-08-29 04:26:14 +0100

    added test file

---
+++ tests/assert_avenger_deaths_are_less_than_returns.sql
@@ -0,0 +1,9 @@
+-- Avengers may return multiple times, but they should not return from the dead!
+select
+    avenger_id,
+    total_returns,
+    total_deaths
+
+from {{ ref('fct_avenger_activity') }}
+
+where total_returns > total_deaths
